c380cef51c98c9c5bce2ed6390dbf081fbcc8916
Lucas Manuel Alface <112022654+Desenvolvedormoz@users.noreply.github.com>
2022-10-09 04:02:53 -0300

    O primeiro desafio foi criar as abas de navegação e instalar as bibliotecas necessárias. Este passo já conclui.

--- App.js
+++ App.js
@@ -1,20 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import TelaXbox from "./Componentes/TelaXbox";
+import TelaPlayStation from "./Componentes/TelaPlayStation";
+import TelaNintendo from "./Componentes/TelaNintendo";
 
+const Tabs = createBottomTabNavigator()
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+   <NavigationContainer>
+    <Tabs.Navigator>
+      <Tabs.Screen name='Xbox' component={TelaXbox}/>
+      <Tabs.Screen name='Play Station' component={TelaPlayStation} />
+      <Tabs.Screen name="Nintendo" component={TelaNintendo}/>
+    </Tabs.Navigator>
+   </NavigationContainer>
+  )
+};
